0f1fbcfe4dcf3369bb8d415957de39ce7f820145
Michael Kimsal <michael@kimsal.com>
2023-03-26 17:18:37 -0400

    fix: migration - add primary key to password token

--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -12,9 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
+            $table->bigIncrements('id');
+            $table->string('email');
             $table->string('token');
             $table->timestamp('created_at')->nullable();
+        });
+        Schema::table('password_reset_tokens', function (Blueprint $table) {
+            $table->dropColumn('id');
+            $table->string('email')->change()->primary();
         });
     }
 
