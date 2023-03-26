bc67532eeff5f7bcbeac28be0b3f6965bf163944
Michael Kimsal <michael@kimsal.com>
2023-03-26 17:57:36 -0400

    chore: force ssl

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,6 +18,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
