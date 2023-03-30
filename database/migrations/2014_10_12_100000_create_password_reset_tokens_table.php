6582350dbda84a90e83cad3bebc18cdc05a17341
Michael Kimsal <michael@kimsal.com>
2023-03-30 18:41:48 -0400

    chore: remove digitalocean-needed sql

--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        DB::statement('SET SESSION sql_require_primary_key=0');
+//        DB::statement('SET SESSION sql_require_primary_key=0');
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
             $table->string('token');
--- database/migrations/2023_02_04_163154_create_sessions_table.php
+++ database/migrations/2023_02_04_163154_create_sessions_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        DB::statement('SET SESSION sql_require_primary_key=0');
+//        DB::statement('SET SESSION sql_require_primary_key=0');
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
             $table->foreignId('user_id')->nullable()->index();
